d091627c800650df6ea925f80c0440b62bfd676d
Vasilis Valatsos <me@aethrvmn.gr>
2025-05-10 23:48:45 +0200

    marginally improved testing

--- tests/demo.py
+++ tests/demo.py
@@ -1,3 +1,4 @@
+import torch.nn as nn
 import torch.optim as optim
 
 from tqdm import tqdm
@@ -6,13 +7,18 @@ from alectors.ppo import Agent
 
 hparams = dict(
     actor_hparams = dict(
-        optimizer=optim.Adam,
-        optimizer_hparams = dict(
+        optimizer_class=optim.Adam,
+        optimizer = dict(
             betas=(0.9, 0.9)
+        ),
+        linear = dict(
+            layers = [100,20],
+            activation_fn=nn.ReLU
         )
+        
     ),
     critic_hparams = dict(
-        optimizer=optim.Adam
+        optimizer_class=optim.Adam
     )
 )
 agent = Agent(
@@ -26,6 +32,15 @@ agent = Agent(
 
 agent.save()
 
+agent = Agent(
+    output_dims=4,
+    batch_size=10,
+    epochs=2,
+    verbose=True,
+    load=True,
+    **hparams
+)
+
 full_state = "You are inside a maze, to escape you must go: Up, Up, Left, Down.".split()
 state = ""
 for i in tqdm(range(len(full_state))):
@@ -34,15 +49,17 @@ for i in tqdm(range(len(full_state))):
     action, probs, vals = agent.choose_action(state)
     agent.store(state, action, probs, vals, 0.11, False, False)
 
+loss = agent.learn()
+
 hparams = dict(
     actor_hparams = dict(
-        optimizer=optim.Adam,
-        optimizer_hparams = dict(
+        optimizer_class=optim.Adam,
+        optimizer = dict(
             betas=(0.9, 0.9)
         )
     ),
     critic_hparams = dict(
-        optimizer_name='adam'
+        optimizer_class=optim.SGD
     )
 )
 agent = Agent(
@@ -50,7 +67,7 @@ agent = Agent(
     batch_size=4,
     epochs=2,
     verbose=True,
-    load=True,
+    load=False,
     **hparams
 )
 
